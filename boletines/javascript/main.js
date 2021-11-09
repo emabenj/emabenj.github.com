c8083946c7ccced5101c6a3e95268d085aca48c0
Enmanuel <emabenj_22@hotmail.com>
2021-11-09 15:10:10 -0500

    add proyecto boletines

---
+++ boletines/javascript/main.js
@@ -0,0 +1,23 @@
+let links = document.querySelectorAll(".close");
+
+links.forEach(function(link){
+	link.addEventListener("click",function(evento){
+		evento.preventDefault();
+
+		let content = document.querySelector(".content");
+
+		content.classList.remove("animate__fadeInDown");
+		content.classList.remove("animate__animated");
+
+		content.classList.add("animate__animated");
+		content.classList.add("animate__fadeOutUp");
+
+		setTimeout(function(){
+			location.href ="./boletines";
+		},600);
+
+		
+
+		return false;
+	});
+});
